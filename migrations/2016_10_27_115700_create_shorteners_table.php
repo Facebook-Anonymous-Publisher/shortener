3bce790875088c3d6287116b582e1ce0f64f9292
BePsvPT <freedom@csie.io>
2016-10-28 11:25:33 +0800

    Fix incorrent filename

--- migrations/2016_10_27_115700_create_shorteners_table.php
+++ migrations/2016_10_27_115700_create_shorteners_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFirewallsTable extends Migration
+class CreateShortenersTable extends Migration
 {
     /**
      * Run the migrations.
